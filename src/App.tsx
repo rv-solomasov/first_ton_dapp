edc0df4e28842c6d2a5145383fdfd2f2852784c4
Rostislav <r.solomasov@servers.com>
2024-07-29 12:47:16 +0300

    pulling from private repo

---
+++ src/App.tsx
@@ -0,0 +1,71 @@
+import {  TonConnectButton } from '@tonconnect/ui-react'
+import './App.css'
+import { useMainContract } from './hooks/useMainContract'
+import { useTonConnect } from './hooks/useTonConnect';
+import { fromNano } from '@ton/core';
+
+// EQAIu12a-WzRXzEGNEW0y03ydFR--zuFQ9LsLlyZumvoB3xY
+
+function App() {
+  const {
+    contract_address,
+    counter_value,
+    recent_sender,
+    owner_address,
+    contract_balance,
+    sendIncrement,
+    sendDeposit,
+    sendWithdrawalRequest
+  } = useMainContract();
+
+  const { connected } = useTonConnect();
+
+  return (
+    <div>
+      <div>
+        <TonConnectButton />
+      </div>
+      <div>
+        <div className='Card'>
+          <b>Our contract Address</b>
+          <div className='Hint'>{contract_address?.slice(0, 30) + "..."}</div>
+          <b>Our contract Balance</b>
+          {contract_balance && (
+            <div className='Hint'>{fromNano(contract_balance)}</div>
+          )}
+        </div>
+
+        <div className='Card'>
+          <b>Counter Value</b>
+          <div>{counter_value ?? "Loading..."}</div>
+        </div>
+      
+      {connected && (
+        <a onClick={() => {
+          sendIncrement()
+        }}>
+          Increment by 1
+        </a>
+      )}
+      <br />
+      {connected && (
+        <a onClick={() => {
+          sendDeposit()
+        }}>
+          Deposit 1 TON
+        </a>
+      )}
+      <br />
+      {connected && (
+        <a onClick={() => {
+          sendWithdrawalRequest()
+        }}>
+          Withdraw 0.5 TON
+        </a>
+      )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
+
+// temp manifest by the course provider
+const manifestUrl = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json';
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <TonConnectUIProvider manifestUrl={manifestUrl}>
+    <App />
+  </TonConnectUIProvider>
+)
